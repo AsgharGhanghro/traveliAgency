9d907373deb6c4d34a9959f965557dff800cfecf
AsgharGhanghro <166389875+AsgharGhanghro@users.noreply.github.com>
2025-05-14 01:33:00 +0500

    first commit

--- Front/src/backend.js
+++ Front/src/backend.js
@@ -1,4 +1,4 @@
-const backend_url = "https://traveling-explore.vercel.app";
+const backend_url = "https://traveli-agency.vercel.app/";
 // const backend_url = "http://localhost:5031";
 
 export default backend_url;
